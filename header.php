0f0f82a5ed221fbac6a78e31bc2ffe34c46ce33c
truemcca <truemcca@gmail.com>
2015-02-12 23:27:09 -0600

    header & Footer
    Header and footer added

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php bloginfo(); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?> "/>
+<meta http.equiv="content_type" content= <?php bloginfo('html_type'); ?> Chartset= <?php bloginfo('chartset'); ?> "><php? wp_head();?>
+<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+	<script src="http://use.edgefonts.net/merriweather.js"></script>
+</head>
+	<body <php body_class( $class ) ; ?>>
+		<header>
+			<h1>Pressed and Squeezed</h1>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>WordPress</li>
+				</ul>
+			</nav>
+		</header>
