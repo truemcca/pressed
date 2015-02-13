f1756220f92091680cb425a1766b26d8c390e537
truemcca <truemcca@gmail.com>
2015-02-12 23:28:04 -0600

    Added
    added header &footer

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+		<section>
+			<img src= "<?php bloginfo('stylesheet_directory'); ?>/images/hero.jpg" />
+
+			<?php if (have_posts () ) : while (have_posts ()) : the_post(); 
+			?>
+			<section>
+				<?php the_content();?>
+				<!--<p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
+				<p>We also make pancakes on the side.</p> -->
+				
+			</section>
+		<?php endwhile; else:?>
+	<?php endif; ?>
+		</section>
+	<?php get_footer(); ?>
